5bdd3aae106fd408e6cba59bb2d9b7adbe50fafc
abuhanaan <adewunmi99@gmail.com>
2022-04-15 03:31:56 +0100

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""addition of two float arguments"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        arguments:
+        a - floating type
+        b - floating type
+    return value: floating type
+    """
+
+    return (a + b)
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,6 @@
+# 0x00-python_variable_annotations
+
+## Resources
+
+- [Python 3 typing documentation](https://alx-intranet.hbtn.io/concepts/554)
+- [MyPy cheat sheet](https://docs.python.org/3/library/typing.html)
