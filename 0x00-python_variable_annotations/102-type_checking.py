0345d80b06c4da307298dfedd5cdd444c17d1316
abuhanaan <adewunmi99@gmail.com>
2022-04-15 03:59:04 +0100

    Advanced

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from typing import List, Tuple
+import math
+"""mypy validation"""
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zoom aray by factor"""
+
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, math.floor(3.0))
