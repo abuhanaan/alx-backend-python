4f9b6a71161619cf12883b4dcdec4868f8af9bc3
abuhanaan <adewunmi99@gmail.com>
2022-04-15 03:44:45 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""converts float n to str and returns it"""
+
+
+def to_str(n: float) -> str:
+    """
+    arguments:
+        n - floating type
+    returns: str type of n
+    """
+
+    return (str(n))
