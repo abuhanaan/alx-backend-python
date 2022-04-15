9c9f2b8806e2f61b575b76a040f35c58baa5c1bd
abuhanaan <adewunmi99@gmail.com>
2022-04-15 03:40:48 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+"""floors a float"""
+
+
+def floor(n: float) -> int:
+    """
+    arguments:
+        n - floating type
+    returns: int type of n(i.e floor of n)
+    """
+
+    return (math.floor(n))
